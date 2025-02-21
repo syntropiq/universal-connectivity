d2fb3cad29d6a02556915b3c764ccc6cacf3ed39
devlux76 <devlux76@icloud.com>
2025-02-21 13:13:52 -0700

    feat: add headless entry point to webpack config and update scripts

--- js-peer/next.config.js
+++ js-peer/next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Add both entry points
+  webpack: (config, { isServer }) => {
+    config.entry = async () => {
+      const entries = await config.entry()
+      
+      // Add headless entry point
+      if (isServer) {
+        entries['./headless'] = './src/index-headless.ts'
+      }
+      
+      return entries
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
