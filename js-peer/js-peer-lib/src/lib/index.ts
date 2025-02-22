3437b749167f01729bdcdd3252096037344174a8
devlux76 <devlux76@icloud.com>
2025-02-21 18:13:37 -0700

    refactor: remove unused configurations and implement logger functionality

---
+++ js-peer/js-peer-lib/src/lib/index.ts
@@ -0,0 +1,4 @@
+export { P2PNode } from './libp2p'
+export type { Libp2pType } from './types'
+export type { DirectMessageEvent } from './direct-message'
+export * from './constants'
--- js-peer/js-peer-standalone/src/app/index.ts
+++
@@ -1,4 +0,0 @@
-export { P2PNode } from '@/lib/libp2p'
-export type { Libp2pType } from '@/lib/types'
-export type { DirectMessageEvent } from '@/lib/direct-message'
-export * from '@/lib/constants'
